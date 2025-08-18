d6f3e94e2d9bf11a8ca876ced542f8a83472cd26
Aniket Botre <aniketbotre007@gmail.com>
2025-08-18 22:59:38 +0530

    feat: Wrap the application body with ThemeProvider and add main tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/context/ThemeContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider>
+          <main className="min-h-screen bg-theme-bg">
+            {children}
+          </main>
+          </ThemeProvider>
       </body>
     </html>
   );
