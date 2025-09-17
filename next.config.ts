bd9eebddadb582423266eafc41b5abc5418c27cd
Aniket Botre <aniketbotre007@gmail.com>
2025-09-17 22:14:26 +0530

    fix: Remove remote patterns from Next.js config images settings

--- next.config.ts
+++ next.config.ts
@@ -3,18 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "media2.dev.to",
-        pathname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "dev-to-uploads.s3.amazonaws.com",
-        pathname: "**",
-      }
-    ],
+    unoptimized: true,
   },
 };
 
